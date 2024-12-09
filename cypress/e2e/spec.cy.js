419737106756611208d700daadb2b5556d7b63ce
Kaique Matheus Sousa <kaique139matheus@gmail.com>
2024-12-09 19:08:51 -0300

    3 testes implementados e adicionada opção para obter o botão de deletar tarefas cumpridas pelo cypress

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,16 +4,16 @@ describe('TODOMvc App', () => {
   })
 
   it('Insere uma tarefa', () => {
-    cy.visit(''); 
+    cy.visit('');
 
     cy.get('[data-cy=todo-input]')
       .type('TP2 de Engenharia de Software{enter}');
 
     cy.get('[data-cy=todos-list]')
       .children()
-      .should('have.length', 1) 
+      .should('have.length', 1)
       .first()
-      .should('have.text', 'TP2 de Engenharia de Software'); 
+      .should('have.text', 'TP2 de Engenharia de Software');
   });
 
   it('Insere e deleta uma tarefa', () => {
@@ -36,7 +36,7 @@ describe('TODOMvc App', () => {
   });
 
   it('Filtra tarefas completas e ativas', () => {
-    cy.visit(''); 
+    cy.visit('');
 
     cy.get('[data-cy=todo-input]')
       .type('TP2 de ES{enter}')
@@ -68,4 +68,94 @@ describe('TODOMvc App', () => {
       .children()
       .should('have.length', 2);
   });
+
+   it('Insere e renomeia uma tarefa', () => {
+    cy.visit('');
+
+    cy.get('[data-cy=todo-input]')
+      .type('TP1{enter}')
+
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 1);
+
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .first()
+      .dblclick()
+      .type('{backspace}3{enter}');
+
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 1);
+
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .first()
+      .should('have.text', "TP3");
+    });
+
+  it('Deleta tarefas completas', () => {
+    cy.visit('');
+
+    cy.get('[data-cy=todo-input]')
+      .type('TP2 de Engenharia de Software{enter}')
+      .type('TP3 de Banco de Dados{enter}')
+      .type('Relatório de Física 4{enter}')
+      .type('Projeto de LAB_ELT2{enter}');
+
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 4);
+
+     cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
+      .first()
+      .click();
+
+     cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
+      .last()
+      .click();
+
+    cy.get('[data-cy="clear-completed-button"]')
+      .click();
+
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 2);
+  });
+
+
+  it('Marca tarefa como ativa e desfaz', () => {
+    cy.visit('');
+
+    cy.get('[data-cy=todo-input]')
+      .type('TP2 de Engenharia de Software{enter}')
+      .type('TP3 de Banco de Dados{enter}')
+
+     cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
+      .first()
+      .click();
+
+    cy.get('[data-cy=filter-completed-link')
+      .click();
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 1)
+      .first()
+      .should('have.text', 'TP2 de Engenharia de Software');
+
+    cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
+      .first()
+      .click();
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 0)
+
+   cy.get('[data-cy=filter-all-link')
+      .click();
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 2);
+  });
+
 });
